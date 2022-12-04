f8265ae13ebd5eb35fab39ecf8a1fda5a5c0df2f
Elizabeth <119602385+VictoriqueXShinku@users.noreply.github.com>
2022-12-04 22:50:02 +0200

    Add files via upload

---
+++ Л11_2(1).cpp
@@ -0,0 +1,38 @@
+﻿//Лабораторна робота 11_2, Завдання 1, Варіант 11//
+
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    cout << "Стан масиву" << endl;
+    const int size = 10;
+    int arr[size];
+    int indexFromMaxElement = 0;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 19 - 9;
+        if (arr[i] > indexFromMaxElement) indexFromMaxElement = i;
+        cout << arr[i] << '\t';
+    }
+    cout << endl;
+
+    for (int i = 0; i < indexFromMaxElement; i++)
+        if (arr[i] < 0) arr[i] = 0;
+
+    cout << "Фінальний результат" << endl;
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << '\t';
+
+    cout << endl << endl;
+    system("pause");
+    return 0;
+}
---
+++ Л11_2(2).cpp
@@ -0,0 +1,45 @@
+﻿//Лабораторна робота 11_2, Завдання 2, Варіант 6//
+
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    const int n = 15;
+    int A[n];
+
+    int x = 1;
+    int maxX = 1;
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = rand() % 10;
+        cout << A[i] << " ";
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (A[i] == A[i + 1])
+        {
+            x++;
+            if (maxX < x)
+            {
+                maxX = x;
+            }
+        }
+        else x = 1;
+    }
+    cout << endl;
+    cout << "Максимальна кількість повторюваних поспіль елементів: " << maxX << endl;
+
+
+    cout << endl << endl;
+    system("pause");
+    return 0;
+}
