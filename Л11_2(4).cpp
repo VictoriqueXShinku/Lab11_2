02c9011484fa3a2ee998594f4028ad44b96cd1b0
Elizabeth <119602385+VictoriqueXShinku@users.noreply.github.com>
2022-12-05 01:32:11 +0200

    Add files via upload

---
+++ Л11_2(4).cpp
@@ -0,0 +1,55 @@
+﻿//Лабораторна робота 11, Завдання 1, Варіант 11, Вправа 4//
+
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <time.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    const int size = 10;
+    int arr[size];
+    int min;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 19 - 9;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    min = abs(arr[0]);
+    for (int i = 0; i < size; i++)
+    {
+        if (min > abs(arr[i]))
+        {
+            min = abs(arr[i]);
+        }
+    }
+    cout << "Мінімальний елемент масиву за модулем: " << min << endl;
+
+    int arr2[size];
+    for (int i = 0; i < size / 2; i++)
+    {
+        arr2[i] = arr[i * 2];
+    }
+    int j = 0;
+    for (int i = size / 2; i < size; i++)
+    {
+        arr2[i] = arr[j * 2 + 1];
+        j++;
+    }
+
+    for (int i = 0; i < size; i++)
+        cout << arr2[i] << " ";
+
+    cout << endl << endl;
+    system("pause");
+    return 0;
+}
